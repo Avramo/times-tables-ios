7c1f004f9f9283e3e23692c8b9031512058a4196
elnatan <elnatan@ravtech.co.il>
2020-01-23 14:08:55 +0200

    Initial Commit

---
+++ Times Tables/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Times Tables
+//
+//  Created by admin on 23/01/2020.
+//  Copyright © 2020 AM Kirsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
